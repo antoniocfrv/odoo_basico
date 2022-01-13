a41c244e626730c3ef7410cfdb4d6f1469e2cf15
antoniocfrv <antoniocfrv@gmail.com>
2022-01-13 21:42:08 +0100

    winLin

--- models/miñasUtilidades.py
+++ models/miñasUtilidades.py
@@ -14,6 +14,15 @@ def rexistra_log(diaHora,ruta,arquivo,contido):
     if os.path.exists(ruta):
         # a Ruta ten que existir e previamente terlle concedidos permisos a odoo para poder facer modificacións
         with open(os.path.join(ruta, arquivo), 'a') as ficheiro:
-            ficheiro.write(diaHora + " " + contido + cadeaTextoSegunPlataforma('\r\n','\n'))
+             ficheiro.write(diaHora + " " + contido + cadeaTextoSegunPlataforma('\r\n','\n'))
+
+        # No caso de non ter permisos de escritura no cartafol e se non quixesemos ver o erro por permiso denegado
+        # try:
+        #     with open(os.path.join(ruta, arquivo), 'a') as ficheiro:
+        #         ficheiro.write(diaHora + " " + contido + cadeaTextoSegunPlataforma('\r\n','\n'))
+        # except OSError:
+        #     pass
+
+
 
 
